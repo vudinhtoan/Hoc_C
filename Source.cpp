3246062ecd799f507fde6aad0458974c726ec386
Toanvd1 <vudinhtoan471995@gmail.com>
2018-12-31 20:59:03 +0700

    first commit

---
+++ Source.cpp
@@ -0,0 +1,20 @@
+#include <conio.h>
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	//int n;
+	int i;
+	cout << "Please enter an integer value:";
+	cin >> i;
+	cout << "The value you entered is " << i;
+	cout << "and its double is " << i * 2 << ".\n";
+	system("pause");
+	return 0;
+	//cout << "nhap gia tri n" ;
+	//cin >> &n;
+
+}
