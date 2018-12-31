9dcf71e28f136193b3f3fdbcdcf3cb563b4269ca
Toanvd1 <vudinhtoan471995@gmail.com>
2018-12-31 21:13:32 +0700

    second commit

--- Source.cpp
+++ Source.cpp
@@ -4,17 +4,34 @@
 using namespace std;
 
 
+// define protocol
+void Tong();
+
 int main()
 {
-	//int n;
+	/*int n;
 	int i;
 	cout << "Please enter an integer value:";
 	cin >> i;
 	cout << "The value you entered is " << i;
 	cout << "and its double is " << i * 2 << ".\n";
+	system("pause"); */
+	Tong();
+
 	system("pause");
 	return 0;
 	//cout << "nhap gia tri n" ;
 	//cin >> &n;
+
+}
+
+void Tong()
+{
+	int a, b;
+		cout << "Please enter an integer value a:";
+		cin >> a ;
+		cout << "Please enter an integer value a:";
+		cin >> b;
+		cout << "Please enter an integer value:" << a + b << endl;
 
 }
