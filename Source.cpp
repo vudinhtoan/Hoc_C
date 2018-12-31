9c0725db870df7cea04a1d0e189b83f56da78d28
Toanvd1 <vudinhtoan471995@gmail.com>
2018-12-31 21:46:04 +0700

    second commit

--- Source.cpp
+++ Source.cpp
@@ -1,11 +1,15 @@
 #include <conio.h>
 #include <stdio.h>
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 
 // define protocol
-void Tong();
+void Tong(int a, int b);
+void tru(int a, int b);
+void nhan(int a, int b);
+void chia(int a, int b);
 
 int main()
 {
@@ -16,7 +20,15 @@ int main()
 	cout << "The value you entered is " << i;
 	cout << "and its double is " << i * 2 << ".\n";
 	system("pause"); */
-	Tong();
+	int a, b;
+	cout << "Please enter an integer value a:";
+	cin >> a;
+	cout << "Please enter an integer value b:";
+	cin >> b;
+	Tong(a,b);
+	tru(a,b);
+	nhan(a, b);
+	chia(a, b);
 
 	system("pause");
 	return 0;
@@ -25,13 +37,44 @@ int main()
 
 }
 
-void Tong()
+void Tong(int a, int b)
 {
-	int a, b;
+		/*int a, b;
 		cout << "Please enter an integer value a:";
 		cin >> a ;
-		cout << "Please enter an integer value a:";
-		cin >> b;
-		cout << "Please enter an integer value:" << a + b << endl;
+		cout << "Please enter an integer value b:";
+		cin >> b;*/
+		cout << "Value a + b:" << a + b << endl;
+
+}
+void tru(int a, int b)
+{
+	/*int a, b;
+	cout << "Please enter an integer value a:";
+	cin >> a;
+	cout << "Please enter an integer value b:";
+	cin >> b;*/
+	cout << "Value a - b:" << a - b << endl;
+}
+
+void nhan(int a, int b)
+{
+	cout << "Value a * b:" << a * b << endl;
+}
+void chia( int a, int b)
+{
+	/*
+	TH1: b = 0; kq = vo nghiem
+	TH2: a > b 
+	*/
+	if (b == 0)
+	{
+		cout << "Program no value" << endl;
+	}
+	else
+	{
+		cout << "Value a/b :" << a / b << endl;
+	}
+	
 
 }
